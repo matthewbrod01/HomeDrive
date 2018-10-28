5ff935867f3b1a07df1f7a9b797a0a0ae0b11289
Amzad Chowdhury <amzad@amzadchowdhury.com>
2018-10-28 05:28:04 -0400

    Massive Updates

--- HomeDrive/View Controllers/OwnerViewController.swift
+++ HomeDrive/View Controllers/OwnerViewController.swift
@@ -11,7 +11,7 @@ import Firebase
 
 class OwnerViewController: UIViewController, UITableViewDataSource, UISearchBarDelegate {
 
-    var ref: DatabaseReference!
+    var db: DatabaseReference!
     @IBOutlet weak var tableView: UITableView!
     
     var bookings: [[String: Any]] = [];
@@ -21,18 +21,70 @@ class OwnerViewController: UIViewController, UITableViewDataSource, UISearchBarD
 
         // Do any additional setup after loading the view.
         tableView.dataSource = self
-        tableView.estimatedRowHeight = 127
-        tableView.rowHeight = UITableView.automaticDimension
+        //tableView.estimatedRowHeight = 127
+        //tableView.rowHeight = UITableView.automaticDimension
         
-        fetchBookings()
+        //let ref = Database.database().reference(withPath: "bookings")
+        
+        //FirebaseApp.configure()
+        
+        db = Database.database().reference().child("owners");
+        let itemsRef = db.child("bookings")
+        
+        //observing the data changes
+        itemsRef.observe(DataEventType.value, with: { (snapshot) in
+            print(snapshot.value as Any)
+            print("Testing")
+            //if the reference have some values
+            if snapshot.childrenCount > 0 {
+                
+                print("Testing")
+                
+                //clearing the list
+                self.bookings.removeAll()
+                
+                //iterating through all the values
+                for booking in snapshot.children.allObjects as! [DataSnapshot] {
+                    //print(booking)
+                    //getting values
+                    let bookingObject = booking.value as? [String: AnyObject]
+                    self.bookings.append((bookingObject ?? nil)!)
+
+                    self.tableView.reloadData()
+                }
+                
+                //reloading the tableview
+                self.tableView.reloadData()
+            }
+        })
+        
+        //fetchBookings()
     }
-    
+    /**
     func fetchBookings() {
-        // code to get data from database
+        self.bookings.removeAll()
         
+        //iterating through all the values
+        for booking in snapshot.children.allObjects as! [DataSnapshot] {
+            print(booking)
+            //getting values
+            let bookingObject = booking.value as? [String: AnyObject]
+            // let name  = bookingObject?["name"]
+            //let license  = bookingObject?["licenseplate"]
+            //let startTime = bookingObject?["startTime"]
+            //let endTime = bookingObject?["endTime"]
+            //let duration = bookingObject?["duration"]
+            
+            self.bookings.append(bookingObject!)
+            
+            self.tableView.reloadData()
+        }
         
-    }
-    
+        //reloading the tableview
+        self.tableView.reloadData()
+        
+    } **/
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return bookings.count
     }
@@ -41,14 +93,18 @@ class OwnerViewController: UIViewController, UITableViewDataSource, UISearchBarD
         let cell = tableView.dequeueReusableCell(withIdentifier: "BookingCell", for: indexPath) as! BookingCell
         
         let booking = bookings[indexPath.row]
-        let title = booking["title"] as! String
-        let overview = booking["overview"] as! String
-        //cell.driverLabel.text = title
-        //cell.licenseLabel.text = license
-        //cell.startTimeLabel = startTime
-        //cell.endTimeLabel = endTime
-        //cell.durationLabel = duration
-        //cell.ratingLabel = rating
+        let title = booking["name"] as! String
+        let overview = booking["licenseplate"] as! String
+        let startTime = booking["startTime"] as! String
+        let endTime = booking["endTime"] as! String
+        let duration = booking["duration"] as! String
+        
+        cell.driverLabel.text = title
+        cell.licenseLabel.text = overview
+        cell.startTimeLabel.text = startTime
+        cell.endTimeLabel.text = endTime
+        cell.durationLabel.text = duration
+        //cell.ratingLabel.text = rating
         
         return cell;
      }
